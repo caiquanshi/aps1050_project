afe145bdbf8a4aafad912499332bf83f0876f984
ViolaSongsy <52792125+ViolaSongsy@users.noreply.github.com>
2023-04-14 01:37:31 -0400

    Add files via upload

---
+++ contracts/petTrack.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+contract petTrack {
+address[16] public owner
+
+mapping (unit => bool) public have_owner;
+
+// Adopting a pet
+function add_owner(string memory _name, string memory _breed, uint _age, string memory _loc, string memory _img) public returns (uint) {
+  require(petId >= 0 && petId <= 15);
+  require(!have_owner[petId]);
+  
+  buyers[petId] = msg.sender;
+
+  return petId;
+}
+function getowner() public view returns (address[16] memory) {
+  return buyers;
+}
+}
